cb65f0c823e7629935b3c51452966d0554efa3bc
anhhoang1233 <125860929+anhhoang1233@users.noreply.github.com>
2024-08-02 09:40:33 +0700

    chore: Update

--- build/server/pure.d.ts
+++ build/server/pure.d.ts
@@ -1,5 +1,6 @@
 export * from "./caches/memoryCache";
 export * from "./transformers/pureTransformer";
+export * from "./resolvers/fetchResolver";
 export * from "./loaders";
 export * from "../utils";
 export * from "../types";
